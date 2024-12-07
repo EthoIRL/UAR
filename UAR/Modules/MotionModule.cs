23ca844122e70564eb6d96b77e33402fdf81a565
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-12-06 17:33:24 -0800

    Add unstable modules for testing

---
+++ UAR/Modules/MotionModule.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace UAR.Modules;
+
+public class MotionModule: GenericOpticalModule<Mat>
+{
+    Matrix<float> matrix = new Matrix<float>(2, 3);
+
+    private (int, int) lastMove = (0, 0);
+    
+    public (int x, int y)? FindMovementFromFlow()
+    {
+        var first = FrameBuffer[0];
+        var second = FrameBuffer[^1];
+
+        // var x = new Mat();
+
+        // float[,] transMatrixFloat = new float[,]
+        // {
+        //     { 1, 0, 5 },
+        //     { 0, 1, 5 }
+        // };
+        
+        //x
+        
+        // Console.WriteLine($"{Program._remoteState.X} {Program._remoteState.Y}");
+        // matrix.Data[0, 2] = -lastMove.Item1;
+        // matrix.Data[1, 2] = -lastMove.Item2;
+        //
+        // CvInvoke.WarpAffine(second, second, matrix, new Size(second.Width, second.Height));
+        //
+        // second = CvInvoke.WarpAffine(second, np.float32([[1, 0, dx], [0, 1, dy]]), (img2.shape[1], img2.shape[0]))
+        
+        var x = first - second;
+        
+        // CvInvoke.AbsDiff(first, second, x);
+        
+        // var neg = first - second;
+        
+        MCvScalar lowerWhite = new MCvScalar(75, 75, 75);
+        MCvScalar upperWhite = new MCvScalar(255, 255, 255);
+
+        CvInvoke.InRange(x, new ScalarArray(lowerWhite), new ScalarArray(upperWhite), x);
+
+        // CvInvoke.BitwiseAnd(x, x, x);
+        
+        CvInvoke.Imshow("Negative", x);
+        CvInvoke.WaitKey(1);
+
+        lastMove = (Program._remoteState.X, Program._remoteState.Y);
+        
+        return (0, 0);
+    }
+
+    public MotionModule(int frameBacklog) : base(frameBacklog)
+    {
+        matrix.Data[0, 0] = 1;
+        matrix.Data[0, 1] = 0;
+        
+        matrix.Data[1, 0] = 0;
+        matrix.Data[1, 1] = 1;
+    }
+}
---
+++ UAR/Modules/PixelMatching.cs
@@ -0,0 +1,87 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace UAR.Modules;
+
+public class PixelMatching : GenericOpticalModule<Mat>
+{
+    private static int KernelWindow = 7;
+    private static readonly float MaximumDissimilarity = 0.000f;
+    
+    public (int x, int y)? FindMovementFromFlow()
+    {
+        var latest = FrameBuffer[0].ToImage<Bgr, byte>();
+        var oldest = FrameBuffer[^1].ToImage<Bgr, byte>();
+
+        var latestData = latest.Data;
+        var oldestData = oldest.Data;
+
+        var xMovement = 0;
+        var yMovement = 0;
+        double divisor = 1;
+
+        for (int x = KernelWindow; x < latest.Rows; x += KernelWindow)
+        {
+            for (int y = KernelWindow; y < latest.Cols; y += KernelWindow)
+            {
+                var blueOldest = oldestData[x, y, 0];
+                var greenOldest = oldestData[x, y, 1];
+                var redOldest = oldestData[x, y, 2];
+                var percentage = (blueOldest + greenOldest + redOldest) / 765.0;
+
+                var pixel = WindowSearch(latestData, oldest, percentage, x, y);
+                if (pixel != null)
+                {
+                    divisor++;
+
+                    xMovement += (pixel.Value.x - x);
+                    yMovement += (pixel.Value.y - y);
+                }
+            }
+        }
+
+        // var deltaX = (int) Math.Ceiling(xMovement / divisor);
+        // var deltaY = (int) Math.Ceiling(yMovement / divisor);
+        
+        var intAvg = HandleOverflow(xMovement / divisor, yMovement / divisor);
+        
+        return (intAvg.x, intAvg.y);
+    }
+
+    public (int x, int y)? WindowSearch(byte[,,] latestData, Image<Bgr, byte> oldest, double percentage, int x, int y)
+    {
+        for (int xWindow = x; xWindow < x + KernelWindow; xWindow++)
+        {
+            if (xWindow >= oldest.Rows)
+            {
+                break;
+            }
+        
+            for (int yWindow = y; yWindow < y + KernelWindow; yWindow++)
+            {
+                if (yWindow >= oldest.Cols)
+                {
+                    continue;
+                }
+        
+                var blueLatest = latestData[xWindow, yWindow, 0];
+                var greenLatest = latestData[xWindow, yWindow, 1];
+                var redLatest = latestData[xWindow, yWindow, 2];
+        
+                var pe = (blueLatest + greenLatest + redLatest) / 765.0;
+                        
+                if (pe == percentage || Math.Abs(pe - percentage) < MaximumDissimilarity)
+                {
+                    return (xWindow, yWindow);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public PixelMatching(int frameBacklog) : base(frameBacklog)
+    {
+        UseFullColor = true;
+    }
+}
