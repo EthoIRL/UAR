1b04cae7f906a4dc1a74d8aa443052ba46869c4d
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-12-06 17:35:06 -0800

    Formatting

--- UAR/Modules/GenericOpticalModule.cs
+++ UAR/Modules/GenericOpticalModule.cs
@@ -1,4 +1,4 @@
-﻿using Emgu.CV.Cuda;
+using Emgu.CV.Cuda;
 
 namespace UAR.Modules;
 
@@ -22,7 +22,7 @@ public abstract class GenericOpticalModule<T> where T : class, new()
         FrameBuffer = new T[frameBacklog];
 
         IsGpuMat = typeof(T) == typeof(GpuMat);
-        
+
         for (int i = 0; i < FrameBuffer.Length; i++)
         {
             FrameBuffer[i] = new T();
@@ -33,32 +33,32 @@ public abstract class GenericOpticalModule<T> where T : class, new()
     {
         for (int i = Backlog - 1; i > 0; i--)
         {
-            FrameBuffer[i] = FrameBuffer[i-1];
+            FrameBuffer[i] = FrameBuffer[i - 1];
         }
-        
+
         FrameBuffer[0] = frame;
     }
-    
+
     protected (int x, int y) HandleOverflow(double x, double y)
     {
-        int intOverflowX = (int) Math.Floor(OverflowX);
-        int intOverflowY = (int) Math.Floor(OverflowY);
-    
+        int intOverflowX = (int)Math.Floor(OverflowX);
+        int intOverflowY = (int)Math.Floor(OverflowY);
+
         x += intOverflowX;
         y += intOverflowY;
-    
+
         OverflowX -= intOverflowX;
         OverflowY -= intOverflowY;
-    
-        var intAvgX = (int) Math.Floor(x);
-        var intAvgY = (int) Math.Floor(y);
-    
+
+        var intAvgX = (int)Math.Floor(x);
+        var intAvgY = (int)Math.Floor(y);
+
         OverflowX += x - intAvgX;
         OverflowY += y - intAvgY;
-    
+
         return (intAvgX, intAvgY);
     }
-    
+
     protected bool SameSign(double value1, double value2)
     {
         return value1 > 0 && value2 > 0 || value1 < 0 && value2 < 0;
